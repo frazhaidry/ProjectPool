f0f4b1cdea76855e40b3641c4056613acdb57f7c
Sheikh-ASif <mdasif74083@gmail.com>
2025-11-01 11:11:57 +0530

    Wrap the <App /> component with the Redux <Provider> and pass it your Redux store

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { Routes, Route } from 'react-router-dom'
-import { AuthProvider } from './contexts/AuthContext'
-import { ProjectProvider } from './contexts/ProjectContext'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Login from './pages/Login'
@@ -14,38 +12,34 @@ import AdminRoute from './components/AdminRoute'
 
 function App() {
   return (
-    <AuthProvider>
-      <ProjectProvider>
-        <div className="min-h-screen bg-gray-50">
-          <Navbar />
-          <main>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/projects" element={<Projects />} />
-              <Route path="/projects/:id" element={<ProjectDetail />} />
-              <Route 
-                path="/my-submissions" 
-                element={
-                  <ProtectedRoute>
-                    <MySubmissions />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route 
-                path="/admin" 
-                element={
-                  <AdminRoute>
-                    <AdminDashboard />
-                  </AdminRoute>
-                } 
-              />
-            </Routes>
-          </main>
-        </div>
-      </ProjectProvider>
-    </AuthProvider>
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/:id" element={<ProjectDetail />} />
+          <Route 
+            path="/my-submissions" 
+            element={
+              <ProtectedRoute>
+                <MySubmissions />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/admin" 
+            element={
+              <AdminRoute>
+                <AdminDashboard />
+              </AdminRoute>
+            } 
+          />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,21 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
+import { Provider } from 'react-redux'
+import { store } from './store/store'  // your redux store
+
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
-    <Toaster 
-      position="top-right"
-      toastOptions={{
-        duration: 4000,
-        style: {
-          background: '#363636',
-          color: '#fff',
-        },
-      }}
-    />
+    <Provider store={store}>   {/* Redux provider */}
+      <App />
+      <Toaster 
+        position="top-right"
+        toastOptions={{
+          duration: 4000,
+          style: {
+            background: '#363636',
+            color: '#fff',
+          },
+        }}
+      />
+    </Provider>
   </BrowserRouter>
 )
