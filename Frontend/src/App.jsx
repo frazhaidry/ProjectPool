f1def216c8891f07e3d28de0ef830d308f76919c
Sheikh-ASif <mdasif74083@gmail.com>
2025-11-04 11:46:12 +0530

    All issues have been resolve now , that occured after the setting up the redux store

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,7 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Routes, Route } from 'react-router-dom'
+
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Login from './pages/Login'
@@ -10,7 +13,24 @@ import AdminDashboard from './pages/AdminDashboard'
 import ProtectedRoute from './components/ProtectedRoute'
 import AdminRoute from './components/AdminRoute'
 
+import { checkAuthStatus } from './store/authSlice'
+
 function App() {
+  const dispatch = useDispatch()
+  const loading = useSelector((state) => state.auth.loading)
+
+  useEffect(() => {
+    dispatch(checkAuthStatus())
+  }, [dispatch])
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="spinner"></div>
+      </div>
+    )
+  }
+
   return (
     <div className="min-h-screen bg-gray-50">
       <Navbar />
@@ -21,21 +41,21 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/projects/:id" element={<ProjectDetail />} />
-          <Route 
-            path="/my-submissions" 
+          <Route
+            path="/my-submissions"
             element={
               <ProtectedRoute>
                 <MySubmissions />
               </ProtectedRoute>
-            } 
+            }
           />
-          <Route 
-            path="/admin" 
+          <Route
+            path="/admin"
             element={
               <AdminRoute>
                 <AdminDashboard />
               </AdminRoute>
-            } 
+            }
           />
         </Routes>
       </main>
@@ -44,3 +64,57 @@ function App() {
 }
 
 export default App
+
+
+
+
+
+
+
+
+// import { Routes, Route } from 'react-router-dom'
+// import Navbar from './components/Navbar'
+// import Home from './pages/Home'
+// import Login from './pages/Login'
+// import Signup from './pages/Signup'
+// import Projects from './pages/Projects'
+// import ProjectDetail from './pages/ProjectDetail'
+// import MySubmissions from './pages/MySubmissions'
+// import AdminDashboard from './pages/AdminDashboard'
+// import ProtectedRoute from './components/ProtectedRoute'
+// import AdminRoute from './components/AdminRoute'
+
+// function App() {
+//   return (
+//     <div className="min-h-screen bg-gray-50">
+//       <Navbar />
+//       <main>
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/login" element={<Login />} />
+//           <Route path="/signup" element={<Signup />} />
+//           <Route path="/projects" element={<Projects />} />
+//           <Route path="/projects/:id" element={<ProjectDetail />} />
+//           <Route 
+//             path="/my-submissions" 
+//             element={
+//               <ProtectedRoute>
+//                 <MySubmissions />
+//               </ProtectedRoute>
+//             } 
+//           />
+//           <Route 
+//             path="/admin" 
+//             element={
+//               <AdminRoute>
+//                 <AdminDashboard />
+//               </AdminRoute>
+//             } 
+//           />
+//         </Routes>
+//       </main>
+//     </div>
+//   )
+// }
+
+// export default App
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,25 +3,63 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import { Provider } from 'react-redux'
-import { store } from './store/store'  // your redux store
-
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from './store/store'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <Provider store={store}>   {/* Redux provider */}
-      <App />
-      <Toaster 
-        position="top-right"
-        toastOptions={{
-          duration: 4000,
-          style: {
-            background: '#363636',
-            color: '#fff',
-          },
-        }}
-      />
+    <Provider store={store}>
+      <PersistGate loading={<div className="spinner" />} persistor={persistor}>
+        <App />
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            duration: 4000,
+            style: {
+              background: '#363636',
+              color: '#fff',
+            },
+          }}
+        />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 )
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import { BrowserRouter } from 'react-router-dom'
+// import { Toaster } from 'react-hot-toast'
+// import { Provider } from 'react-redux'
+// import { store } from './store/store'  // your redux store
+
+// import App from './App.jsx'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <BrowserRouter>
+//     <Provider store={store}>   {/* Redux provider */}
+//       <App />
+//       <Toaster 
+//         position="top-right"
+//         toastOptions={{
+//           duration: 4000,
+//           style: {
+//             background: '#363636',
+//             color: '#fff',
+//           },
+//         }}
+//       />
+//     </Provider>
+//   </BrowserRouter>
+// )
